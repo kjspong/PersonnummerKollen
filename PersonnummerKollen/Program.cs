2f3b38d32f72711c714add9e2e6c6e794c68b7e1
kjspong <kjspong@protonmail.com>
2023-10-15 21:27:08 +0200

    Add program files

---
+++ PersonnummerKollen/Program.cs
@@ -0,0 +1,73 @@
+namespace PersonnummerKollen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Välkommen till PersonnummerKollen!\n\nKnappa in någons födelsedag (ÅÅMMDD): ");
+            int fodelseDag = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange de tre första av de fyra sista: ");
+            int treAvFyra = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write($"Du har angivit följande information: {fodelseDag}-{treAvFyra}");
+            Console.WriteLine();
+
+            //Slå samman födelsedag och de tre första av de fyra sista
+            int fodelseDagOchTreAvFyra = Toolbox.Concat(fodelseDag, treAvFyra);
+
+            //Splitta det sammanslagna numret till en array
+            var personNummer = fodelseDagOchTreAvFyra.ToString().Select(x => (int)char.GetNumericValue(x)).ToArray();
+
+            // Värden från Luhnalgoritmen
+            int[] luhn = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+
+            // Produkten av luhn * personNummer
+            int[] product = new int[9];
+
+            // Beräkna luhn * personNummer och lägg produkten i lista
+            List<int> productList = new();
+
+            for (int i = 0; i < luhn.Length; i++)
+            {
+                product[i] = luhn[i] * personNummer[i];
+                productList.Add(product[i]);
+            }
+
+            // Addera talen om tvåsiffrig produkt och lägg summan i lista
+            List<int> productSumList = new();
+            int productSum = 0;
+
+            for (int i = 0; i < productList.Count; i++)
+            {
+                // === Knyckt från stackoverflow ====
+                while (productList[i] != 0)                     // Kör så länge något värde i listan inte är 0
+                {
+                    productSum += productList[i] % 10;          // Beräkna [i] mod 10 (spara "decimalen")
+                    productList[i] /= 10;                       // Beräkna [i] / 10 (spara heltalet)
+                }                                               // Kvar blir 0
+
+                productSumList.Add(productSum);
+                productSum = 0;
+            }
+
+            // Summera talen
+            int horizSum = 0;
+
+            foreach (int i in productSumList)
+            {
+                horizSum += i;
+            }
+
+            // Beräkna kontrollsiffran och skriv ut
+            int kontrollSiffra = 0;
+            kontrollSiffra = Toolbox.RoundUp(horizSum) - horizSum;
+
+            Console.WriteLine();
+            Console.Write($"Kontrollsiffran är: {kontrollSiffra}");
+            Console.WriteLine();
+            Console.WriteLine($"Det fullständiga personnumret är: {fodelseDag}-{treAvFyra}{kontrollSiffra}");
+        }
+    }
+}
---
+++ PersonnummerKollen/Toolbox.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnummerKollen
+{
+    internal class Toolbox
+    {
+        //Slå samman födelsedag och de tre första av de fyra sista
+        public static int Concat(int fodelseDag, int treAvFyra)
+        {
+            string s1 = fodelseDag.ToString();
+            string s2 = treAvFyra.ToString();
+            string s = s1 + s2;
+            int fodelseDagOchTreAvFyra = int.Parse(s);
+            return fodelseDagOchTreAvFyra;
+        }
+
+        // Beräkna närmsta 10-tal uppåt
+        public static int RoundUp(int horizSum)
+        {
+            // === Knyckt från stackoverflow ====
+            double horizSumRoundUp = (double)horizSum / 10;     // Casta till double och dela med 10 för att få ett decimaltal
+            horizSumRoundUp = Math.Ceiling(horizSumRoundUp);    // Runda av uppåt till närmsta heltal om talet inte är jämt
+            horizSumRoundUp = horizSumRoundUp * 10;             // Multiplicera med 10 igen för att få närmsta 10-tal uppåt
+            return (int)horizSumRoundUp;
+        }
+    }
+}
